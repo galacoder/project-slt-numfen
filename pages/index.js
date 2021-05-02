5cba081a56b399431e00c48242905518254cbfb0
Sang Le <lengobaosang@gmail.com>
2021-05-02 22:07:26 +0700

    update to all dependences to latest versions

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 // @generated: @expo/next-adapter@2.1.52
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Text style={styles.text}>
+        Welcome to Sang Le Tech App Numerology App 👋
+      </Text>
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 16,
