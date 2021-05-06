50dd4e1e86460e9ce2ff19618eed835f905edf4e
Sang Le <lengobaosang@gmail.com>
2021-05-06 15:33:03 +0700

    successfully installed eslint & prettier

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,6 @@ import styled from "styled-components/native";
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
-    // </View>
     <Container>
       <Title>Hello world from styled component</Title>
       <Smartie />
@@ -32,14 +29,3 @@ const Smartie = styled.ImageBackground.attrs({
   height: 220px;
   transform: scale(0.5);
 `;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   text: {
-//     fontSize: 16,
-//   },
-// });
