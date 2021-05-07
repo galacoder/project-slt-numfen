a1ec5a2b082a355204fc04885dd1813c692590fb
Sang Le <lengobaosang@gmail.com>
2021-05-08 06:48:30 +0700

    notion render work for web but not ios android

--- pages/screens/results.js
+++ pages/screens/results.js
@@ -1,16 +1,20 @@
 import * as React from "react";
 import styled from "styled-components/native";
 import { Button, ProgressBar, Colors } from "react-native-paper";
+import { ScrollView } from "react-native";
 
 import Smartie from "../../components/Smartie";
 import ContentBox from "../../components/ContentBox";
+import NotionPage from "../../lib/getNotionPage";
 
 export default function Result({ navigation }) {
   return (
-    <>
+    <ScrollView>
       <Pagination />
       <Smartie />
       <ContentBox text="This is result page" />
+      <NotionPage />
+
       <NavBtnContainer>
         <BackButton onPress={() => navigation.navigate("Landing")}>
           Back
@@ -19,15 +23,13 @@ export default function Result({ navigation }) {
           Next
         </NextButton>
       </NavBtnContainer>
-    </>
+    </ScrollView>
   );
 }
 
 const Pagination = styled(ProgressBar).attrs({
   progress: 0.5,
 })``;
-
-const Title = styled.Text``;
 
 const NavBtnContainer = styled.View`
   flex-direction: row;
